828f6d099eb8e4087310812391171b0341308e39
VishwasPG12 <22c57.vishwas@sjec.ac.in>
2025-12-23 20:17:29 +0530

    Update server.js to use .env for MongoDB credentials

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,15 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
+const uri = process.env.MONGODB_URI;
 mongoose
-  .connect("mongodb+srv://pgvishwas21:Expense123@cluster0.kc7n7.mongodb.net/?appName=Cluster0")
+  .connect(uri)
   .then(() => console.log("MongoDB Atlas connected"))
   .catch(err => console.error(err));
-
 
 // Routes
 app.use("/api/expenses", require("./routes/expenseRoutes.js"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
